4a8e73e110ddf6ee280ad09cbbed761c3b618413
codeytimey <87842664+codeytimey@users.noreply.github.com>
2022-09-29 19:15:09 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+song = "";
+status = "";
+objects = [];
+
+function preload() {
+    song = loadSound("alert_alert.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    
+    document.getElementById("status").innerHTML = "Status : Detecting Humans";
+}
+
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+    if (status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Human Detected";
+
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if (objects[i].label == "person") {
+                document.getElementById("number_of_objects").innerHTML = "Baby Found";
+                console.log("stop");
+                song.stop();
+            }
+            else {
+                document.getElementById("number_of_objects").innerHTML = "Baby Not Found";
+                console.log(play);
+                song.play();
+            }
+        }
+        if (objects.length == 0) {
+            document.getElementById("number_of_objects").innerHTML = "Baby Not Found";
+            console.log("play");
+            song.play();
+        }
+    }
+}
